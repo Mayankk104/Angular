9d3e7d28768c36f1d4da4e5dff1c433474259d36
Mayank kumar <Mayankk104@gmail.com>
2021-12-13 10:53:26 +0530

    create user model interface

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,10 @@
+export interface localUser {
+    user: {
+        _id: string,
+        name: string,
+        age: number,
+        email: string,
+        __v: number
+    },
+    token: string
+}
